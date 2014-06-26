d78407c6d8a0246c1adda01aa15370ae85903136
destiny14 <mstambor@googlemail.com>
2014-06-26 16:52:25 +0100

    Made prototype working. Added npm-log to gitignore

--- ircclient.js
+++ ircclient.js
@@ -3,12 +3,62 @@ var irc = require('irc');
 function run(io) {
 	io.on('connection', function(socket){
 	    console.log('user connected');
-	    var client = irc.Client();
+	    var client;
+	    var channel;
+	    socket.on('client connect', function(data){
+	    	client = new irc.Client(data.server, data.nickname, {
+		    	channels: [data.channel],
+			});
+	    	channel = data.channel;
+	    	client.addListener('message' + data.channel, function(from, message){
+	    		socket.emit('message recieved', {
+	    			frm: from,
+	    			msg: message
+	    		});
+	    	});
+
+	    	client.addListener('motd', function(motd){
+	    		socket.emit('message recieved', {
+	    			frm: 'motd',
+	    			msg: motd
+	    		});
+	    	});
+
+	    	client.addListener('topic', function(channel, topic, nick, message){
+	    		socket.emit('message recieved', {
+	    			frm: 'topic',
+	    			msg: topic + " set by " + nick
+	    		});
+	    	});
+	    	client.addListener('error', function(message) {
+	    		socket.emit('message recieved', {
+	    			frm: "ERROR",
+	    			msg: message.args[1]
+	    		})
+				console.log('error: ', message);
+			});
+	    });
+	 
 	    socket.on('chat message', function(msg){
-	        console.log("message: " + msg);
+	    	console.log(msg);
+	        if (typeof client !== 'undefined' && client) {
+	        	client.say(channel, msg);
+	        	socket.emit('message recieved', {
+	        		frm: 'Me',
+	        		msg: msg
+	        	});
+	        } else {
+	        	console.log("there was no client connected");
+	        }
 	    });
 	    socket.on('disconnect', function(){
 	        console.log("user disconnected");
+	        if (typeof client !== 'undefined' && client) {
+	        	client.disconnect();	
+	        } else {
+	        	console.log("there was no client connected");
+	        }
+	        
 	    });
 	});
 }
